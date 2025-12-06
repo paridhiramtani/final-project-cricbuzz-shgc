42bdf6b1539231e1df5d5cf43ccac32e7bd45061
paridhiramtani <ramtaniparidhi@gmail.com>
2025-12-06 16:25:28 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,12 +3,11 @@ require('dotenv').config()
 const http = require('http')
 const app = require('./app')
 
-const port = process.env.port
-const host = process.env.host
-
+const port = process.env.PORT || 8080
+const host = process.env.HOST || 'localhost'
 
 const server = http.createServer(app)
 
-server.listen(port,()=>{
-    console.log(`Server started as ${host}:${port} , Players Server`);
+server.listen(port, () => {
+    console.log(`Server started at ${host}:${port}`)
 })
